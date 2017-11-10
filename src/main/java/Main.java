0ccbb659b7e3f57d089829eaff75e21b75cbbb01
George Tsotzolas <tsotzolas@gmail.com>
2017-11-10 12:39:04 +0200

    Add to write the map file and send it to the server

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,13 +6,12 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         String user = "user";
         String password = "6973533175";
         String host = "83.212.102.71";
         int port = 2204;
 
-        String remoteFile = "/home/user/Desktop/restart.sh";
+        String remoteFile = "/etc/haproxy/scripts/ip.map";
 
         try {
             JSch jsch = new JSch();
@@ -26,12 +25,62 @@ public class Main {
 
 
 
+
+            //Read file from remote server via ssh
+            ChannelSftp sftpChannel = (ChannelSftp) session.openChannel("sftp");
+            sftpChannel.connect();
+            System.out.println("SFTP Channel created.");
+            InputStream out = null;
+            out = sftpChannel.get(remoteFile);
+            BufferedReader br = new BufferedReader(new InputStreamReader(out));
+            String line;
+
+            FileWriter fstreamWrite = new FileWriter("ip.map");
+            BufferedWriter out1 = new BufferedWriter(fstreamWrite);
+
+            //Parse the file and change the server's names.
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                String ip = (line.split(" ")[0]);
+                String server = (line.split(" ")[1]);
+                //Change the server name
+                //Todo Here must put the logic to change the server name
+                if (line.contains("server1")){
+                    line = line.replace(server, "server2");
+
+                }else{
+                    line = line.replace(server, "server1");
+                }
+                System.out.println(line);
+                System.out.println("------------------");
+
+                out1.write(line.toString());
+                out1.write("\n");
+
+            }
+
+
+            out1.close();
+            out.close();
+
+
+            //Take the temp file
+            File newFile = new File("ip.map");
+            //Put the file in the remote server
+            sftpChannel.put(newFile.getAbsolutePath(),"/etc/haproxy/scripts");
+            //Delete the temp file
+            newFile.delete();
+
+
+            //Command Execution
             Channel channel=session.openChannel("exec");
-            ((ChannelExec)channel).setCommand("echo 6973533175 | sudo -S service haproxy restart");
+            System.out.println("Execute command 'sudo service haproxy restart'.");
+            ((ChannelExec)channel).setCommand("echo 6973533175 | sudo -S service haproxy reload");
+
             channel.setInputStream(null);
             ((ChannelExec)channel).setErrStream(System.err);
 
-            InputStream in=channel.getInputStream();
+            InputStream in = channel.getInputStream();
             channel.connect();
             byte[] tmp=new byte[1024];
             while(true){
@@ -46,8 +95,12 @@ public class Main {
                 }
                 try{Thread.sleep(1000);}catch(Exception ee){}
             }
+
+            in.close();
+            br.close();
             channel.disconnect();
             session.disconnect();
+            System.out.println("Command Executed");
             System.out.println("DONE");
 
 
